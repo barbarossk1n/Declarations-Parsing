b1355e92c0c1c89294421b46022b7236eeb9399e
barbarossk1n <166558954+barbarossk1n@users.noreply.github.com>
2024-07-12 02:59:40 +0300

    DECLARATION DOWNLOAD AND RENAMING

--- main_script/libraries.py
+++ main_script/libraries.py
@@ -18,8 +18,12 @@ import json
 PARSING LIBRARIES
 '''
 from selenium import webdriver
+from selenium.webdriver import Chrome
+
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+
+
 
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
--- main_script/main.py
+++ main_script/main.py
@@ -197,16 +197,19 @@ class Parsing:
                 self.dict_projects = json.load(f)
 
             for id in list(self.dict_projects.keys())[3:4]:
-                pdf_directory = self.progrm_path + self.slash + 'projects'
-                os.chdir(pdf_directory + self.slash + 'pdf')
+                pdf_directory = self.progrm_path + self.slash + 'projects' + self.slash + 'pdf'
+                os.chdir(pdf_directory)
 
-                if id not in os.listdir(pdf_directory + self.slash + 'pdf'):
+                # create the folder for the project to download there all the declarations
+                if id not in os.listdir(pdf_directory):
                     os.mkdir(id)
                 
                 os.chdir(id)
+                if 'download' not in os.listdir():
+                    os.mkdir('download')                    
 
                 # all links have the same template
-                project_link = TEMPLATE_LINK + '/' + id
+                project_link = TEMPLATE_LINK + id
 
                 # run the link of the single project to 
                 driver = webdriver.Chrome(service=self.ser)
@@ -224,8 +227,70 @@ class Parsing:
                 list_declarations_subdates = self.find_special_elements(driver = driver, 
                                                                      parameter = single_declaration_date, attempt = attempt)
                 list_declarations_dates = [date.text for date in list_declarations_subdates]
+
+                # combine the declarations links for the download and the dates at which the declarations were published
+                dict_declarations = {}
+                for i in range(len(list_declarations_dates) - 1, -1, -1):
+                    dict_declarations[str(list_declarations_dates[i])] = list_declarations_hrefs[i]
+
+                # creating special options for the browser in order to download to this folder
+                id_path = pdf_directory + self.slash + id
+
+                prefs = {
+                         'download.default_directory': id_path + self.slash + 'download',
+                         'download.prompt_for_download': False,
+                         'download.directory_upgrade': True,
+                         'safebrowsing.enabled': False,
+                         'safebrowsing.disable_download_protection': True   
+                        }
                 
+                self.opt.add_experimental_option('prefs', prefs)
+                additional_params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': id_path + self.slash + 'download'}}
                 
+                wb_driver_updated = webdriver.Chrome(service=self.ser, options=self.opt)
+                wb_driver_updated.command_executor._commands['send_command'] = ('POST', '/session/$sessionId/chromium/send_command')
+                wb_driver_updated.execute('send_command', additional_params)
+
+                # declarations downloading - set default max number of the declarations: 12
+                declarations_number = 12
+                for i in range(min(len(list_declarations_dates), declarations_number)):
+                    try:
+                        os.chdir(pdf_directory)
+
+                        wb_driver_updated.get(dict_declarations[list_declarations_dates[i]])
+
+                        count_try = 0
+                        list_download = os.listdir(id_path + self.slash + 'download')
+
+                        while len(list_download) == 0 and count_try <= 100:
+                            print('file is not downloading!')
+                            time.sleep(0.1)
+                            list_download = os.listdir(id_path + self.slash + 'download')
+                            
+                            count_try += 1
+
+                        if len(list_download) != 0:
+                            while list_download[0].endswith('.tmp') or list_download[0].endswith('.crdownload'):
+                                print('file is still downloading!')
+                                time.sleep(0.1)
+                                list_download = os.listdir(id_path + self.slash + 'download')
+
+                            os.chdir(id_path)
+                            os.chdir('Download')
+                            os.rename(list_download[0], id + '_' + list_declarations_dates[i] + '.pdf')
+
+                            shutil.move(id_path + self.slash + 'download' + self.slash + id + '_' + list_declarations_dates[i] + '.pdf', id_path)
+
+                            print('Declaration for {} was downloaded!'.format(list_declarations_dates[i]))
+
+                        else:
+                            print('Declaration for {} was not downloaded!'.format(list_declarations_dates[i]))
+
+                    except:
+                        continue
+                
+                wb_driver_updated.close()
+                driver.close()
 
 
 if __name__ == '__main__':
